b13d4abf2df94276577573db41c12e48cd0e710f
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-06-01 20:30:16 +0700

    menyamakan output

---
+++ Pertemuan14/BinaryTree08.java
@@ -0,0 +1,163 @@
+package Pertemuan14;
+
+public class BinaryTree08 {
+    Node08 root;
+
+    public BinaryTree08() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void add(Mahasiswa08 mahasiswa) {
+        Node08 newNode = new Node08(mahasiswa);
+        if (isEmpty()) {
+            root = newNode;
+        } else {
+            Node08 current = root;
+            Node08 parent = current;
+            while (true) {
+                //Node08 parent = current;
+                parent = current;
+                if (mahasiswa.ipk < current.mahasiswa.ipk) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean find(double ipk) {
+        Node08 current = root;
+        while (current != null) {
+            if (current.mahasiswa.ipk == ipk) {
+                return true;
+            } else if (ipk > current.mahasiswa.ipk) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return false;
+    }
+
+    public void traversePreOrder(Node08 node) {
+        if (node != null) {
+            node.mahasiswa.tampilInformasi();
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traverseInOrder(Node08 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            node.mahasiswa.tampilInformasi();
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node08 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            node.mahasiswa.tampilInformasi();
+        }
+    }
+
+    public Node08 getSuccessor(Node08 del) {
+        Node08 successor = del.right;
+        Node08 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    public void delete(double ipk) {
+        if (isEmpty()) {
+            System.out.println("Binary tree kosong");
+            return;
+        }
+        Node08 parent = root;
+        Node08 current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (current.mahasiswa.ipk == ipk) {
+                break;
+            } else if (ipk < current.mahasiswa.ipk) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (ipk > current.mahasiswa.ipk) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        if (current == null) {
+            System.out.println("Data tidak ditemukan");
+            return;
+        } else {
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {
+                Node08 successor = getSuccessor(current);
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                }
+                successor.left = current.left;
+            }
+        }
+    }
+}
---
+++ Pertemuan14/Node08.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node08 {
+    Mahasiswa08 mahasiswa;
+    Node08 left, right;
+
+    public Node08() {
+    }
+
+    public Node08(Mahasiswa08 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        this.left = null; 
+        this.right = null;
+    }
+}
